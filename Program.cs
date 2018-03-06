717a213af482405bf822ba82ab944e9a0980b6eb
Dominik Czajkowski <dominik.czajkowski96@gmail.com>
2018-03-06 15:22:08 +0100

    final commit

--- Program.cs
+++ Program.cs
@@ -8,49 +8,47 @@ namespace game_of_life
 		public static void Main (string[] args)
 		{
 			game g = new game();
-			g.print ();
-			g.next_step ();
-			g.print ();
+			while (true) {
+				g.next_step ();
+				g.print ();
+				Console.ReadKey ();
+				Console.Clear ();
+			}
 
 		}
 	}
 
 	class game
 	{
-		private bool[,] arr = new bool[3, 3];
-		private bool[,] arr2 = new bool[3, 3];
+		private bool[,] arr = new bool[10, 10];
+		private bool[,] arr2 = new bool[10, 10];
 
 		public game()
 		{
-			for (int i = 0; i < 3; i++)
-				for (int j = 0; j < 3; j++)
+			for (int i = 0; i < 10; i++)
+				for (int j = 0; j < 10; j++)
 					arr [i, j] = false;
 			arr [0, 0] = true;
 			arr [0, 1] = true;
 			arr [0, 2] = true;
+			arr [3, 0] = true;
+			arr [3, 1] = true;
+			arr [3, 2] = true;
 		}
 
 		public void print()
 		{
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 10; j++) {
 					if (arr [i, j] == true)
 						Console.Write ("O");
 					else
-						Console.Write ("X");
+						Console.Write (" ");
 					
 				}
 				Console.WriteLine ("");
 			}
-		/*	for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-
-					Console.Write (this.count_neighb(i,j));
-
-
-				}
-				Console.WriteLine ("");
-			}           */
+          
 		}
 
 		private int count_neighb(int i, int j)
@@ -60,26 +58,26 @@ namespace game_of_life
 					if( i-1 >=0)
 					if(arr[i-1,j] == true)
 						cnt++;
-					if( i+1 <= 2)
+					if( i+1 <= 9)
 					if(arr[i+1,j] == true)
 						cnt++;
 					if( j-1 >= 0)
 					if(arr[i,j-1] == true)
 						cnt++;
-					if( j+1 <= 2)
+					if( j+1 <= 9)
 					if(arr[i,j+1] == true)
 						cnt++;
-			if((j+1 <=2) && (i-1 >=0))
+			if((j+1 <=9) && (i-1 >=0))
 				if(arr[i-1,j+1])
 				cnt++;
-			if((j+1 <=2) && (i+1 <=2))
+			if((j+1 <=9) && (i+1 <=9))
 			if(arr[i+1,j+1])
 				cnt++;
 			if((j-1 >=0) && (i-1 >=0))
 			if(arr[i-1,j-1])
 				cnt++;
 					
-			if((j-1 >=0) && (i+1 <=2))
+			if((j-1 >=0) && (i+1 <=9))
 			if(arr[i+1,j-1])
 				cnt++;
 
@@ -93,8 +91,8 @@ namespace game_of_life
 		}
 		public void next_step()
 		{
-			for (int i = 0; i < 3; i++) 
-				for (int j = 0; j < 3; j++) 
+			for (int i = 0; i < 10; i++) 
+				for (int j = 0; j < 10; j++) 
 				{
 					if (arr [i, j] == true) {
 						if (this.count_neighb (i, j) < 2 || this.count_neighb (i, j) > 3)
